29af64d509ae489ee60557a710df410e35c11c3d
danomatika <danomatika@gmail.com>
2016-08-15 17:17:32 -0600

    CocoaAsyncSocket sources now downloaded with CocoaHTTPServer again

--- scripts/update_httpserver.sh
+++ scripts/update_httpserver.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 #
-# Dan Wilcox <danomatika@gmail.com> 2012
+# this script automatically updates the sources for the CocoaHTTPServer ios library
+#
+# as long as the download link is formatted in the same way and folder
+# structure are the same, this script should *just work*
+#
+# Dan Wilcox <danomatika@gmail.com> 2013
 #
 
 WD=$(dirname $0)
-DEST=../libs/CocoaHTTPServer
+SRC_DIR=CocoaHTTPServer
+DEST_DIR=../libs/CocoaHTTPServer
 
 ###
 
@@ -14,17 +20,13 @@ cd $WD
 git clone git://github.com/robbiehanson/CocoaHTTPServer.git
 
 # make folder
-mkdir -p $DEST
-
-# don't need a second copy of CocoaAsyncSocket
-rm -rf CocoaHTTPServer/Vendor/CocoaAsyncSocket
+mkdir -p $DEST_DIR
 
 # copy sources
-cp -Rv CocoaHTTPServer/Core $DEST
-cp -Rv CocoaHTTPServer/Extensions $DEST
-cp -Rv CocoaHTTPServer/Vendor $DEST
-cp -Rv CocoaHTTPServer/LICENSE.txt $DEST
+cp -Rv $SRC_DIR/Core $DEST_DIR/
+cp -Rv $SRC_DIR/Extensions $DEST_DIR/
+cp -Rv $SRC_DIR/Vendor $DEST_DIR/
+cp -Rv $SRC_DIR/LICENSE.txt $DEST_DIR/
 
 # cleanup
-rm -rfv CocoaHTTPServer
-
+rm -rfv $SRC_DIR
