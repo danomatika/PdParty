8ea3f8e09e9a5374b67eb15feb106bc7cff98aff
danomatika <danomatika@gmail.com>
2016-10-28 14:02:40 -0600

    cleaned Externals.m

--- libs/pd-externals/Externals.m
+++ libs/pd-externals/Externals.m
@@ -18,21 +18,7 @@ void moog_tilde_setup();
 void stripdir_setup();
 
 // mrpeach
-//void httpreceive_setup();
-//void httpreq_setup();
 void midifile_setup();
-//void packOSC_setup();
-//void pipelist_setup();
-//void routeOSC_setup();
-//void tcpclient_setup();
-//void tcpreceive_setup();
-//void tcpsend_setup();
-//void tcpserver_setup();
-//void udpreceive_setup();
-//void udpreceive_tilde_setup();
-//void udpsend_setup();
-//void udpsend_tilde_setup();
-//void unpackOSC_setup();
 
 // rj
 void rj_accum_setup();
@@ -51,21 +37,7 @@ void rj_zcr_tilde_setup();
 	stripdir_setup();
 
 	// mrpeach
-	//httpreceive_setup();
-	//httpreq_setup();
 	midifile_setup();
-	//packOSC_setup();
-	//pipelist_setup();
-	//routeOSC_setup();
-	//tcpclient_setup();
-	//tcpreceive_setup();
-	//tcpsend_setup();
-	//tcpserver_setup();
-	//udpreceive_setup();
-	//udpreceive_tilde_setup();
-	//udpsend_setup();
-	//udpsend_tilde_setup();
-	//unpackOSC_setup();
 	
 	// rj
 	rj_accum_setup();
